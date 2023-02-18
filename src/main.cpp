34a06899d7abd2b715309a8e6b3b6862668d6ad4
R. ALOUI <aloui.radhouane@gmail.com>
2023-02-18 09:37:57 +0100

    Read from touch pin

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,13 @@
 #include <Arduino.h>
 
+#define TOUCH_PIN   4
+
 void setup(){
   Serial.begin(115200);
   Serial.print("Hello world");
 }
 
 void loop(){
-  
+  Serial.printf("%5d", touchRead(TOUCH_PIN));
+  delay(500);
 }
